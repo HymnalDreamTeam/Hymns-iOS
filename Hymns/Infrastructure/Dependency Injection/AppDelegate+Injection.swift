f616d84bd1ac7135b37de82ef9db608e20d1b4c1
Luke <skywalkerdude@gmail.com>
2021-10-26 15:09:16 -0700

    Fix deprecated Resolver call

--- Hymns/Infrastructure/Dependency Injection/AppDelegate+Injection.swift
+++ Hymns/Infrastructure/Dependency Injection/AppDelegate+Injection.swift
@@ -6,7 +6,7 @@ import Foundation
 extension Resolver: ResolverRegistering {
 
     #if DEBUG
-    static let mock = Resolver(parent: main)
+    static let mock = Resolver(child: main)
     #endif
 
     public static func registerAllServices() {
