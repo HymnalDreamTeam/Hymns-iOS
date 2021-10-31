2270cd5f82a1dc57b65946dbd59bdd401314cc0e
Luke <skywalkerdude@gmail.com>
2021-10-30 22:54:25 -0700

    Fix BottomBarSnapshots

--- HymnsSnapshotTests/BottomBarSnapshots.swift
+++ HymnsSnapshotTests/BottomBarSnapshots.swift
@@ -19,7 +19,7 @@ class BottomBarSnapshots: XCTestCase {
         let bottomBar = DisplayHymnBottomBar(dialogModel: Binding<DialogViewModel<AnyView>?>(
             get: {self.dialog},
             set: {self.dialog = $0}), viewModel: viewModel).padding().ignoresSafeArea()
-        assertVersionedSnapshot(matching: bottomBar, as: .swiftUiImage())
+        assertVersionedSnapshot(matching: bottomBar, as: .image(layout: .fixed(width: 200, height: 50)))
     }
 
     func test_oneButton() {
@@ -28,7 +28,7 @@ class BottomBarSnapshots: XCTestCase {
         let bottomBar = DisplayHymnBottomBar(dialogModel: Binding<DialogViewModel<AnyView>?>(
             get: {self.dialog},
             set: {self.dialog = $0}), viewModel: viewModel).padding().ignoresSafeArea()
-        assertVersionedSnapshot(matching: bottomBar, as: .image())
+        assertVersionedSnapshot(matching: bottomBar, as: .image)
     }
 
     func test_twoButtons() {
@@ -37,7 +37,7 @@ class BottomBarSnapshots: XCTestCase {
         let bottomBar = DisplayHymnBottomBar(dialogModel: Binding<DialogViewModel<AnyView>?>(
             get: {self.dialog},
             set: {self.dialog = $0}), viewModel: viewModel).padding().ignoresSafeArea()
-        assertVersionedSnapshot(matching: bottomBar, as: .image())
+        assertVersionedSnapshot(matching: bottomBar, as: .image)
     }
 
     func test_maximumButtons() {
@@ -75,6 +75,6 @@ class BottomBarSnapshots: XCTestCase {
         let bottomBar = DisplayHymnBottomBar(dialogModel: Binding<DialogViewModel<AnyView>?>(
             get: {self.dialog},
             set: {self.dialog = $0}), viewModel: viewModel).padding().ignoresSafeArea()
-        assertVersionedSnapshot(matching: bottomBar, as: .image())
+        assertVersionedSnapshot(matching: bottomBar, as: .image)
     }
 }
