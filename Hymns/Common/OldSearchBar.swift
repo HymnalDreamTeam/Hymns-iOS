28ce733f399d82cca39c097e7a59d4c63ec08c94
Luke <skywalkerdude@gmail.com>
2022-02-01 15:58:53 -0800

    Add ability to toggle keyboard input type in the search screen

--- Hymns/Common/OldSearchBar.swift
+++ Hymns/Common/OldSearchBar.swift
@@ -1,49 +1,17 @@
 import SwiftUI
 
-/**
- * Custom search bar that will animate in a `Cancel` button when activated/selected.
- * https://stackoverflow.com/questions/56490963/how-to-display-a-search-bar-with-swiftui
- */
-@available(iOS 15, *)
-struct SearchBar: View {
+/// Search Bar for devices running earlier than iOS 15. This is a workaround because of https://developer.apple.com/forums/thread/688678, where @FocusState causes
+/// iOS 14 devices to crash even though it's wrapped with @available. Delete this class when the minimum supported iOS version is iOS 15+.
+struct OldSearchBar: View {
 
     @Binding var searchText: String
     @Binding var searchActive: Bool
     let placeholderText: String
 
-    @State var searchMode: SearchMode = SearchMode(rawValue: UserDefaults.standard.integer(forKey: "search_mode")) ?? .keyword {
-        willSet {
-            UserDefaults.standard.set(newValue.rawValue, forKey: "search_mode")
-        }
-    }
-    // Use FocusState here as a workaround since SwiftUI doesn't yet provide a way to reload the keyboard on a keyboard type change.
-    // https://stackoverflow.com/questions/70934381/reload-textfield-keyboard-in-swiftui
-    @FocusState var focused
-
     var body: some View {
         HStack {
             HStack {
-                if searchActive {
-                    Button(action: {
-                        focused = false
-                        if searchMode == .keyword {
-                            searchMode = .number
-                        } else {
-                            searchMode = .keyword
-                        }
-                        focused = true
-                    }, label: {
-                        if searchMode == .keyword {
-                            Image(systemName: "textformat.abc").foregroundColor(.accentColor)
-                        } else {
-                            Image(systemName: "textformat.123").foregroundColor(.accentColor)
-                        }
-                    }).font(.system(size: smallButtonSize)).padding(.leading, 6)
-                }
-                TextField(placeholderText, text: $searchText)
-                    .keyboardType(searchMode == .keyword ? .asciiCapable : .asciiCapableNumberPad)
-                    .foregroundColor(.primary)
-                    .focused($focused)
+                TextField(placeholderText, text: $searchText).foregroundColor(.primary)
                 if !self.searchText.isEmpty {
                     Button(action: {
                         self.searchText = ""
@@ -82,15 +50,8 @@ struct SearchBar: View {
     }
 }
 
-@available(iOS 15, *)
-enum SearchMode: Int {
-    case keyword
-    case number
-}
-
 #if DEBUG
-@available(iOS 15, *)
-struct SearchBar_Previews: PreviewProvider {
+struct OldSearchBar_Previews: PreviewProvider {
     static var previews: some View {
         let placeholderText = "Search by numbers or words"
         var emptySearchText = ""
@@ -101,17 +62,20 @@ struct SearchBar_Previews: PreviewProvider {
         let searchInactiveBinding = Binding<Bool>(
             get: {searchInactive},
             set: {searchInactive = $0})
-        let searchInactiveBar = SearchBar(searchText: emptySearchTextBinding, searchActive: searchInactiveBinding, placeholderText: placeholderText)
+        let searchInactiveBar = OldSearchBar(searchText: emptySearchTextBinding, searchActive: searchInactiveBinding,
+                                             placeholderText: placeholderText)
         var searchActive = true
         let searchActiveBinding = Binding<Bool>(
             get: {searchActive},
             set: {searchActive = $0})
-        let searchActiveBar = SearchBar(searchText: emptySearchTextBinding, searchActive: searchActiveBinding, placeholderText: placeholderText)
+        let searchActiveBar = OldSearchBar(searchText: emptySearchTextBinding, searchActive: searchActiveBinding,
+                                           placeholderText: placeholderText)
         var searchText = "Who let the dogs out?"
         let searchTextBinding = Binding<String>(
             get: {searchText},
             set: {searchText = $0})
-        let searchTextBar = SearchBar(searchText: searchTextBinding, searchActive: searchActiveBinding, placeholderText: placeholderText)
+        let searchTextBar = OldSearchBar(searchText: searchTextBinding, searchActive: searchActiveBinding,
+                                         placeholderText: placeholderText)
         return
             Group {
                 searchInactiveBar.previewDisplayName("inactive")
