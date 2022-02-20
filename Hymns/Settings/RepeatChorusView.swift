59ad6371134a070d07823d3c8861a1c9aac18dd0
Luke <skywalkerdude@gmail.com>
2022-02-19 17:51:19 -0800

    Fix error in RepeatChorusView that made the blurb in iOS 14 not warp but ellipsis instead.

--- Hymns/Settings/RepeatChorusView.swift
+++ Hymns/Settings/RepeatChorusView.swift
@@ -16,6 +16,10 @@ struct RepeatChorusView: View {
             VStack(alignment: .leading, spacing: 5) {
                 Text("Repeat chorus", comment: "Title for settings item to repeat the chorus.")
                 Text("For songs with only one chorus, repeat the chorus after every verse.", comment: "Subtitle for settings item to repeat the chorus.")
+                    // Text keeps ellipsizing at one line, so need to do something like this to allow it to wrap. Error only happened
+                    // in iOS 14.4 and 14.5, so likely we can remove this when we stop supporting iOS 14.
+                    // https://stackoverflow.com/questions/56505929/the-text-doesnt-get-wrapped-in-swift-ui/59277022#59277022
+                    .fixedSize(horizontal: false, vertical: true)
                     .font(.caption)
             }
         }.padding()
