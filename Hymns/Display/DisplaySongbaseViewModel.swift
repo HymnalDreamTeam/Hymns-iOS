224c92cc541e54ccbfe8d47d769ace429eab33e1
Luke <skywalkerdude@gmail.com>
2022-03-13 22:12:43 -0600

    Add an overlay to show the guitar url if it exists

--- Hymns/Display/DisplaySongbaseViewModel.swift
+++ Hymns/Display/DisplaySongbaseViewModel.swift
@@ -1,12 +1,56 @@
 import Combine
 import Foundation
+import PDFKit
 import Resolver
 
 class DisplaySongbaseViewModel: ObservableObject {
 
     @Published var chords: [ChordLine] = [ChordLine]()
+    @Published var pdfDocument: PDFDocument?
 
-    init(chords: [ChordLine]) {
+    private let analytics: AnalyticsLogger
+    private let backgroundQueue: DispatchQueue
+    private let guitarUrl: URL?
+    private let mainQueue: DispatchQueue
+    private let preloader: PDFLoader
+
+    init(analytics: AnalyticsLogger = Resolver.resolve(),
+         backgroundQueue: DispatchQueue = Resolver.resolve(name: "background"),
+         chords: [ChordLine],
+         guitarUrl: URL?,
+         mainQueue: DispatchQueue = Resolver.resolve(name: "main"),
+         preloader: PDFLoader = Resolver.resolve()) {
+        self.backgroundQueue = backgroundQueue
+        self.analytics = analytics
         self.chords = chords
+        self.guitarUrl = guitarUrl
+        self.mainQueue = mainQueue
+        self.preloader = preloader
+    }
+
+    func loadPdf() {
+        guard let guitarUrl = guitarUrl else {
+            return
+        }
+
+        if let preloadedDoc = preloader.get(url: guitarUrl) {
+            self.analytics.logDisplayMusicPdfSuccess(url: guitarUrl)
+            self.pdfDocument = preloadedDoc
+        } else {
+            self.analytics.logLoadMusicPdf(url: guitarUrl)
+            backgroundQueue.async {
+                if let document = PDFDocument(url: guitarUrl) {
+                    self.analytics.logDisplayMusicPdfSuccess(url: guitarUrl)
+                    self.mainQueue.async {
+                        self.pdfDocument = document
+                    }
+                } else {
+                    self.analytics.logDisplayMusicPdfFailed(url: guitarUrl)
+                    self.mainQueue.async {
+                        self.pdfDocument = nil
+                    }
+                }
+            }
+        }
     }
 }
