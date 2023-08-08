165e4db76f9ad2cab69d7976137340117efbbeaf
Luke <skywalkerdude@gmail.com>
2023-08-07 23:15:50 -0700

    Add to Snapshot+Extensions to work with Xcode Cloud by bundling screenshots with the binary instead of looking inside the src directory. For more details: https://jaanus.com/snapshot-testing-xcode-cloud/

--- HymnsSnapshotTests/Test Utilities/Snapshot+Extensions.swift
+++ HymnsSnapshotTests/Test Utilities/Snapshot+Extensions.swift
@@ -13,52 +13,77 @@ extension Snapshotting where Value: SwiftUI.View, Format == UIImage {
         precision: Float = 1,
         size: CGSize? = nil,
         traits: UITraitCollection = .init()) -> Snapshotting {
-        Snapshotting<UIViewController, UIImage>.image(
-            drawHierarchyInKeyWindow: drawHierarchyInKeyWindow,
-            precision: precision,
-            size: size,
-            traits: traits)
+            Snapshotting<UIViewController, UIImage>.image(
+                drawHierarchyInKeyWindow: drawHierarchyInKeyWindow,
+                precision: precision,
+                size: size,
+                traits: traits)
             .pullback(UIHostingController.init(rootView:))
-    }
+        }
 }
 
 /**
  * Asserts a snapshot based on the current OS system version.
  */
-public func assertVersionedSnapshot<Value, Format>(
-    matching value: @autoclosure () throws -> Value,
-    as snapshotting: Snapshotting<Value, Format>,
-    named name: String? = nil,
-    record recording: Bool = false,
-    snapshotDirectory: String? = nil,
-    timeout: TimeInterval = 5,
-    file: StaticString = #file,
-    testName: String = #function,
-    line: UInt = #line) {
-
+public func assertVersionedSnapshot<Value, Format>(matching value: @autoclosure () throws -> Value,
+                                                   as snapshotting: Snapshotting<Value, Format>,
+                                                   record recording: Bool = false,
+                                                   timeout: TimeInterval = 5,
+                                                   file: StaticString = #file,
+                                                   testName: String = #function,
+                                                   line: UInt = #line) {
     let osVersion = UIDevice.current.systemVersion
     let fileUrl = URL(string: "\(file)")!
     let fileName = fileUrl.deletingPathExtension().lastPathComponent
-    let defaultSnapshotDirectory = fileUrl
-        .deletingLastPathComponent()
-        .appendingPathComponent("__Snapshots__")
-        .appendingPathComponent(osVersion)
-        .appendingPathComponent(fileName)
-        .absoluteString
 
-    let failure = verifySnapshot(
-        matching: try value(),
-        as: snapshotting,
-        named: name,
-        record: recording,
-        snapshotDirectory: snapshotDirectory ?? defaultSnapshotDirectory,
-        timeout: timeout,
-        file: file,
-        testName: testName,
-        line: line
-    )
-    guard let message = failure else { return }
-    XCTFail(message, file: file, line: line)
+    let locales = [Locale(identifier: "en_US")]
+    do {
+        try locales.forEach { locale in
+            let snapshotDirectory: String
+            // Check bundled snapshot so it will work with Xcode Cloud
+            // https://jaanus.com/snapshot-testing-xcode-cloud/
+            // https://gist.github.com/jaanus/7e14b31f7f445435aadac09d24397da8
+            if let bundledSnapshotDirectory = Bundle.main.resourceURL?
+                .appendingPathComponent("PlugIns")
+                .appendingPathComponent("HymnsSnapshotTests.xctest")
+                .appendingPathComponent("__Snapshots__")
+                .appendingPathComponent(osVersion)
+                .appendingPathComponent(fileName)
+                .path(percentEncoded: false),
+               case let bundledSnapshot = "\(bundledSnapshotDirectory)\(sanitizePathComponent(testName)).\(locale.identifier).png",
+               FileManager.default.fileExists(atPath: bundledSnapshot) {
+                snapshotDirectory = bundledSnapshotDirectory
+            } else {
+                snapshotDirectory = fileUrl
+                    .deletingLastPathComponent()
+                    .appendingPathComponent("__Snapshots__")
+                    .appendingPathComponent(osVersion)
+                    .appendingPathComponent(fileName)
+                    .absoluteString
+            }
+            if let failureMessage = verifySnapshot(
+                matching: try value(),
+                as: snapshotting,
+                named: locale.identifier,
+                record: recording,
+                snapshotDirectory: snapshotDirectory,
+                timeout: timeout,
+                file: file,
+                testName: testName,
+                line: line) {
+                XCTFail(failureMessage, file: file, line: line)
+            }
+        }
+    } catch {
+        XCTFail(String(describing: error), file: file, line: line)
+    }
+}
+
+// Copied from swift-snapshot-testing
+func sanitizePathComponent(_ string: String) -> String {
+  return string
+    .replacingOccurrences(of: "\\W+", with: "-", options: .regularExpression)
+    .replacingOccurrences(of: "^-|-$", with: "", options: .regularExpression)
 }
 
 extension SwiftUI.View {
