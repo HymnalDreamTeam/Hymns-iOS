11dca5b2753b67ac2474501a57911d964dade497
skywalkerdude <skywalkerdude@gmail.com>
2025-01-04 15:10:24 -0800

    Fix ci_post_clone.sh script per instructions in https://developer.apple.com/documentation/xcode/writing-custom-build-scripts#Create-the-CI-scripts-directory

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Disable package and macro fingerprint validation to enable the SwiftLint plugin during the build process.
+# Typo in "Validation" is intended: https://github.com/realm/SwiftLint/issues/5624
 defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidatation -bool YES
 defaults write com.apple.dt.Xcode IDESkipMacroFingerprintValidation -bool YES
-
