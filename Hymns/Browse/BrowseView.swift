6f2bee185a2c902cdaa2323ac7997f235cc561a4
skywalkerdude <skywalkerdude@gmail.com>
2025-01-11 17:22:05 -0800

    Migrate BrowseView to use Prefire Snapshots

--- Hymns/Browse/BrowseView.swift
+++ Hymns/Browse/BrowseView.swift
@@ -1,3 +1,4 @@
+import Prefire
 import Resolver
 import SwiftUI
 
@@ -27,9 +28,19 @@ struct BrowseView: View {
 }
 
 #if DEBUG
-struct BrowseView_Previews: PreviewProvider {
+struct BrowseView_Previews: PreviewProvider, PrefireProvider {
     static var previews: some View {
-        BrowseView()
+        let classicSelectedViewModel = BrowseViewModel()
+        classicSelectedViewModel.currentTab = .classic(Text("classic songs").maxSize().eraseToAnyView())
+        let classicSelected = BrowseView(viewModel: classicSelectedViewModel)
+
+        let allSelectedViewModel = BrowseViewModel()
+        allSelectedViewModel.currentTab = .all(Text("all songs").maxSize().eraseToAnyView())
+        let allSelected = BrowseView(viewModel: allSelectedViewModel)
+        return Group {
+            classicSelected.previewDisplayName("classic selected")
+            allSelected.previewDisplayName("all selected")
+        }
     }
 }
 #endif
