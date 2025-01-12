518b1d61c198704dec039a9e4efa1e895e7e47bb
skywalkerdude <skywalkerdude@gmail.com>
2025-01-11 19:01:43 -0800

    Migrate ScriptureView to use Prefire Snapshots

--- Hymns/Browse/ScriptureView.swift
+++ Hymns/Browse/ScriptureView.swift
@@ -1,3 +1,4 @@
+import Prefire
 import SwiftUI
 
 struct ScriptureView: View {
@@ -5,6 +6,12 @@ struct ScriptureView: View {
     @State fileprivate var isExpanded = false
 
     let viewModel: ScriptureViewModel
+
+    init(viewModel: ScriptureViewModel,
+         isExpanded: Bool = false) {
+        self.isExpanded = isExpanded
+        self.viewModel = viewModel
+    }
 
     var body: some View {
         Section {
@@ -27,13 +34,16 @@ struct ScriptureView: View {
 }
 
 #if DEBUG
-struct ScriptureView_Previews: PreviewProvider {
+struct ScriptureView_Previews: PreviewProvider, PrefireProvider {
     static var previews: some View {
         let viewModel = ScriptureViewModel(book: .malachi,
                                            scriptureSongs: [ScriptureSongViewModel(reference: "4:2", title: "But unto you who fear My name", hymnIdentifier: HymnIdentifier(hymnType: .newSong, hymnNumber: "410")),
                                                             ScriptureSongViewModel(reference: "5:1", title: "totally made up song", hymnIdentifier: HymnIdentifier(hymnType: .cebuano, hymnNumber: "98"))])
         viewModel.scriptureSongs = [ScriptureSongViewModel(reference: "General", title: "here is the title", hymnIdentifier: PreviewHymnIdentifiers.cupOfChrist)]
-        return ScriptureView(viewModel: viewModel).previewLayout(.sizeThatFits)
+        return List {
+            ScriptureView(viewModel: viewModel)
+            ScriptureView(viewModel: viewModel, isExpanded: true).listRowSeparator(.hidden)
+        }.listStyle(.plain).background(Color(.systemBackground))
     }
 }
 #endif
