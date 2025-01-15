c9906f01905bb42affb3f8060b720883a703c6f4
skywalkerdude <skywalkerdude@gmail.com>
2025-01-14 16:39:37 -0800

    Migrate DisplayHymnView to use Prefire Snapshots

--- Hymns/Display/DisplayHymnView.swift
+++ Hymns/Display/DisplayHymnView.swift
@@ -1,5 +1,6 @@
-import SwiftUI
+import Prefire
 import Resolver
+import SwiftUI
 
 struct DisplayHymnView: View {
 
@@ -198,9 +199,10 @@ struct HymnNotExistsView: View {
 }
 
 #if DEBUG
-struct DisplayHymnView_Previews: PreviewProvider {
+struct DisplayHymnView_Previews: PreviewProvider, PrefireProvider {
     static var previews: some View {
-        let hymn: UiHymn = UiHymn(hymnIdentifier: HymnIdentifier(hymnType: .classic, hymnNumber: "23"), title: "sdf", lyrics: [VerseEntity]())
+        let hymn: UiHymn = UiHymn(hymnIdentifier: HymnIdentifier(hymnType: .classic, hymnNumber: "23"),
+                                  title: "sdf", lyrics: [VerseEntity]())
 
         let loadingViewModel = DisplayHymnViewModel(hymnToDisplay: HymnIdentifier(hymnType: .blueSongbook, hymnNumber: "998"))
         loadingViewModel.isLoaded = false
@@ -212,64 +214,75 @@ struct DisplayHymnView_Previews: PreviewProvider {
         errorViewModel.isFavorited = false
         let error = DisplayHymnView(viewModel: errorViewModel)
 
-        let classic40ViewModel = DisplayHymnViewModel(hymnToDisplay: PreviewHymnIdentifiers.hymn40)
-        classic40ViewModel.isLoaded = true
-        classic40ViewModel.title = "Hymn 40"
-        classic40ViewModel.isFavorited = true
-        let classic40LyricsViewModel = HymnLyricsViewModel(hymnToDisplay: PreviewHymnIdentifiers.hymn40, lyrics: classic40_preview.lyrics.verses)!
-        classic40ViewModel.currentTab = .lyrics(HymnLyricsView(viewModel: classic40LyricsViewModel).eraseToAnyView())
-        classic40ViewModel.tabItems = [classic40ViewModel.currentTab, .music(Text("%_PREVIEW_% Music here").eraseToAnyView())]
-        classic40ViewModel.bottomBar = DisplayHymnBottomBarViewModel(hymnToDisplay: PreviewHymnIdentifiers.hymn40, hymn: hymn)
-        let classic40 = DisplayHymnView(viewModel: classic40ViewModel)
+        let lyricsViewModel = HymnLyricsViewModel(hymnToDisplay: PreviewHymnIdentifiers.hymn40,
+                                                  lyrics: classic40_preview.lyrics.verses)!
 
-        let classic1151ViewModel = DisplayHymnViewModel(hymnToDisplay: PreviewHymnIdentifiers.hymn1151)
-        classic1151ViewModel.isLoaded = true
-        classic1151ViewModel.title = "Hymn 1151"
-        classic1151ViewModel.isFavorited = false
-        let classic1151LyricsViewModel = HymnLyricsViewModel(hymnToDisplay: PreviewHymnIdentifiers.hymn1151, lyrics: classic1151_preview.lyrics.verses)!
-        classic1151ViewModel.currentTab = .lyrics(HymnLyricsView(viewModel: classic1151LyricsViewModel).maxSize().eraseToAnyView())
-        classic1151ViewModel.tabItems = [classic1151ViewModel.currentTab, .music(Text("%_PREVIEW_% Music here").eraseToAnyView())]
-        classic1151ViewModel.bottomBar = DisplayHymnBottomBarViewModel(hymnToDisplay: PreviewHymnIdentifiers.hymn1151, hymn: hymn)
-        let classic1151 = DisplayHymnView(viewModel: classic1151ViewModel)
+        // Set hymnNumber to be outside the range of existing hymns, otherwise the preview will try to fetch the real song
+        // instead of use fake data, making it non-deterministic.
+        let singleTabViewModel = DisplayHymnViewModel(hymnToDisplay: HymnIdentifier(hymnType: .classic, hymnNumber: "4099"))
+        singleTabViewModel.isLoaded = true
+        singleTabViewModel.title = "Hymn 4099"
+        singleTabViewModel.isFavorited = true
+        singleTabViewModel.currentTab = .lyrics(HymnLyricsView(viewModel: lyricsViewModel).eraseToAnyView())
+        singleTabViewModel.tabItems = [singleTabViewModel.currentTab]
+        let singleTab = DisplayHymnView(viewModel: singleTabViewModel)
 
-        let classic1151MusicViewModel = DisplayHymnViewModel(hymnToDisplay: PreviewHymnIdentifiers.hymn1151)
-        classic1151MusicViewModel.isLoaded = true
-        classic1151MusicViewModel.title = "Hymn 1151"
-        classic1151MusicViewModel.isFavorited = false
-        classic1151MusicViewModel.currentTab = .music(Text("%_PREVIEW_% Music here").eraseToAnyView())
-        classic1151MusicViewModel.tabItems = [.lyrics(HymnLyricsView(viewModel: classic1151LyricsViewModel).maxSize().eraseToAnyView()),
-                                              classic1151MusicViewModel.currentTab]
-        classic1151MusicViewModel.bottomBar = DisplayHymnBottomBarViewModel(hymnToDisplay: PreviewHymnIdentifiers.hymn1151, hymn: hymn)
-        let classic1151Music = DisplayHymnView(viewModel: classic1151MusicViewModel)
+        // Set hymnNumber to be outside the range of existing hymns, otherwise the preview will try to fetch the real song
+        // instead of use fake data, making it non-deterministic.
+        let multipleTabsViewModel = DisplayHymnViewModel(hymnToDisplay: HymnIdentifier(hymnType: .classic, hymnNumber: "5100"))
+        multipleTabsViewModel.isLoaded = true
+        multipleTabsViewModel.title = "Super new song"
+        multipleTabsViewModel.isFavorited = true
+        multipleTabsViewModel.currentTab = .lyrics(HymnLyricsView(viewModel: lyricsViewModel).eraseToAnyView())
+        multipleTabsViewModel.tabItems = [multipleTabsViewModel.currentTab, .music(Text("%_PREVIEW_% Music here").eraseToAnyView())]
+        let multipleTabs = DisplayHymnView(viewModel: multipleTabsViewModel)
 
-        let classic1334ViewModel = DisplayHymnViewModel(hymnToDisplay: PreviewHymnIdentifiers.hymn1334)
-        classic1334ViewModel.isLoaded = true
-        classic1334ViewModel.title = "Hymn 1334"
-        classic1334ViewModel.isFavorited = nil
-        let classic1334LyricsViewModel = HymnLyricsViewModel(hymnToDisplay: PreviewHymnIdentifiers.hymn1334, lyrics: classic1334_preview.lyrics.verses)!
-        classic1334ViewModel.currentTab = .lyrics(HymnLyricsView(viewModel: classic1334LyricsViewModel).maxSize().eraseToAnyView())
-        classic1334ViewModel.tabItems = [HymnTab]()
-        let classic1334BottomBarViewModel = DisplayHymnBottomBarViewModel(hymnToDisplay: PreviewHymnIdentifiers.hymn1151, hymn: hymn)
-        let classic1334SongInfoDialogViewModel = SongInfoDialogViewModel(hymnToDisplay: PreviewHymnIdentifiers.hymn1151,
-                                                                         hymn: UiHymn(hymnIdentifier: PreviewHymnIdentifiers.hymn1151, title: "", lyrics: nil, author: "MC"))!
-        classic1334SongInfoDialogViewModel.songInfo = [SongInfoViewModel(type: .hymnCode, values: ["value1", "value2"])]
-        classic1334BottomBarViewModel.buttons = [
+        // Set hymnNumber to be outside the range of existing hymns, otherwise the preview will try to fetch the real song
+        // instead of use fake data, making it non-deterministic.
+        let multipleTabsMusicViewModel = DisplayHymnViewModel(hymnToDisplay: HymnIdentifier(hymnType: .classic, hymnNumber: "5100"))
+        multipleTabsMusicViewModel.isLoaded = true
+        multipleTabsMusicViewModel.title = "Super new song music"
+        multipleTabsMusicViewModel.currentTab = .music(Text("%_PREVIEW_% Music here").eraseToAnyView())
+        multipleTabsMusicViewModel.tabItems = [.lyrics(HymnLyricsView(viewModel: lyricsViewModel).eraseToAnyView()),
+                                               multipleTabsMusicViewModel.currentTab]
+        let multipleTabsMusic = DisplayHymnView(viewModel: multipleTabsMusicViewModel)
+
+        // Set hymnNumber to be outside the range of existing hymns, otherwise the preview will try to fetch the real song
+        // instead of use fake data, making it non-deterministic.
+        let fullSongViewModel = DisplayHymnViewModel(hymnToDisplay: HymnIdentifier(hymnType: .classic, hymnNumber: "5100"))
+        fullSongViewModel.isLoaded = true
+        fullSongViewModel.title = "Hymn 1334"
+        fullSongViewModel.isFavorited = nil
+        let fullSongLyricsViewModel = HymnLyricsViewModel(hymnToDisplay: PreviewHymnIdentifiers.hymn1334,
+                                                          lyrics: classic1334_preview.lyrics.verses)!
+        fullSongViewModel.currentTab = .lyrics(HymnLyricsView(viewModel: fullSongLyricsViewModel).eraseToAnyView())
+        fullSongViewModel.tabItems = [fullSongViewModel.currentTab,
+                                      .music(Text("%_PREVIEW_% Music here").eraseToAnyView())]
+        let fullSongBottomBarViewModel = DisplayHymnBottomBarViewModel(hymnToDisplay: PreviewHymnIdentifiers.hymn1151, hymn: hymn)
+        let fullSongSongInfoDialogViewModel = SongInfoDialogViewModel(hymnToDisplay: PreviewHymnIdentifiers.hymn1151,
+                                                                      hymn: UiHymn(hymnIdentifier: PreviewHymnIdentifiers.hymn1151,
+                                                                                   title: "", lyrics: nil, author: "MC"))!
+        fullSongSongInfoDialogViewModel.songInfo = [SongInfoViewModel(type: .hymnCode, values: ["value1", "value2"])]
+        fullSongBottomBarViewModel.buttons = [
             .share("Shareable lyrics"),
-            .languages([SingleSongResultViewModel(stableId: "Empty title view", title: "language", destinationView: EmptyView().eraseToAnyView())]),
-            .relevant([SingleSongResultViewModel(stableId: "Empty relevant view", title: "relevant", destinationView: EmptyView().eraseToAnyView())]),
+            .languages([SingleSongResultViewModel(stableId: "Empty title view", title: "language",
+                                                  destinationView: EmptyView().eraseToAnyView())]),
+            .relevant([SingleSongResultViewModel(stableId: "Empty relevant view", title: "relevant",
+                                                 destinationView: EmptyView().eraseToAnyView())]),
             .musicPlayback(AudioPlayerViewModel(url: URL(string: "https://www.hymnal.net/Hymns/NewSongs/mp3/ns0767.mp3")!)),
-            .songInfo(classic1334SongInfoDialogViewModel)
+            .songInfo(fullSongSongInfoDialogViewModel)
         ]
-        classic1334ViewModel.bottomBar = classic1334BottomBarViewModel
-        let classic1334 = DisplayHymnView(viewModel: classic1334ViewModel)
+        fullSongViewModel.bottomBar = fullSongBottomBarViewModel
+        let fullSong = DisplayHymnView(viewModel: fullSongViewModel)
+
         return Group {
             loading.previewDisplayName("loading")
             error.previewDisplayName("error")
-            classic40.previewDisplayName("classic 40")
-            classic1151.previewDisplayName("classic 1151")
-            classic1151Music.previewDisplayName("classic 1151 music")
-            classic1334.previewDisplayName("classic 1134")
-        }
+            singleTab.previewDisplayName("single tab")
+            multipleTabs.previewDisplayName("multiple tabs")
+            multipleTabsMusic.previewDisplayName("multiple tabs music")
+            fullSong.previewDisplayName("full song")
+        }.snapshot(delay: 0.5)
     }
 }
 #endif
