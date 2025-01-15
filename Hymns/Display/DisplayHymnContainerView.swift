2e8318db8a7b24e42bdab65ea49dce3e5d95b841
skywalkerdude <skywalkerdude@gmail.com>
2025-01-14 18:09:30 -0800

    Migrate DisplayHymnContainerView to use Prefire Snapshots

--- Hymns/Display/DisplayHymnContainerView.swift
+++ Hymns/Display/DisplayHymnContainerView.swift
@@ -1,5 +1,6 @@
-import SwiftUIPager
+import Prefire
 import SwiftUI
+import SwiftUIPager
 
 struct DisplayHymnContainerView: View {
 
@@ -30,3 +31,51 @@ struct DisplayHymnContainerView: View {
         }
     }
 }
+
+#if DEBUG
+struct DisplayHymnContainerView_Previews: PreviewProvider, PrefireProvider {
+    static var previews: some View {
+        let classic40LyricsViewModel = HymnLyricsViewModel(hymnToDisplay: PreviewHymnIdentifiers.hymn40,
+                                                           lyrics: classic40_preview.lyrics.verses)!
+        // Set hymnNumber to be outside the range of existing hymns, otherwise the preview will try to fetch the real song
+        // instead of use fake data, making it non-deterministic.
+        let classic40ViewModel = DisplayHymnViewModel(hymnToDisplay: HymnIdentifier(hymnType: .classic, hymnNumber: "4000"))
+        classic40ViewModel.isLoaded = true
+        classic40ViewModel.title = "Classic 40"
+        classic40ViewModel.currentTab = .lyrics(HymnLyricsView(viewModel: classic40LyricsViewModel).eraseToAnyView())
+        classic40ViewModel.tabItems = [classic40ViewModel.currentTab]
+
+        let classic1334LyricsViewModel = HymnLyricsViewModel(hymnToDisplay: PreviewHymnIdentifiers.hymn1334,
+                                                             lyrics: classic1334_preview.lyrics.verses)!
+        // Set hymnNumber to be outside the range of existing hymns, otherwise the preview will try to fetch the real song
+        // instead of use fake data, making it non-deterministic.
+        let classic1334ViewModel = DisplayHymnViewModel(hymnToDisplay: HymnIdentifier(hymnType: .classic, hymnNumber: "13340"))
+        classic1334ViewModel.isLoaded = true
+        classic1334ViewModel.title = "Classic 1334"
+        classic1334ViewModel.currentTab = .lyrics(HymnLyricsView(viewModel: classic1334LyricsViewModel).eraseToAnyView())
+        classic1334ViewModel.tabItems = [classic1334ViewModel.currentTab]
+
+        let loadingViewModel = DisplayHymnContainerViewModel(hymnToDisplay: HymnIdentifier(hymnType: .classic, hymnNumber: "23"))
+        let loading = DisplayHymnContainerView(viewModel: loadingViewModel)
+
+        let oneHymnViewModel = DisplayHymnContainerViewModel(hymnToDisplay: HymnIdentifier(hymnType: .classic, hymnNumber: "40"))
+        oneHymnViewModel.hymns = [classic40ViewModel]
+        let oneHymn = DisplayHymnContainerView(viewModel: oneHymnViewModel)
+
+        let zeroHymnsViewModel = DisplayHymnContainerViewModel(hymnToDisplay: HymnIdentifier(hymnType: .classic, hymnNumber: "40"))
+        zeroHymnsViewModel.hymns = []
+        let zeroHymns = DisplayHymnContainerView(viewModel: zeroHymnsViewModel)
+
+        let multipleHymnsViewModel = DisplayHymnContainerViewModel(hymnToDisplay: HymnIdentifier(hymnType: .classic, hymnNumber: "1334"))
+        multipleHymnsViewModel.hymns = [classic40ViewModel, classic1334ViewModel]
+        let multipleHymns = DisplayHymnContainerView(viewModel: multipleHymnsViewModel)
+
+        return Group {
+            loading.previewDisplayName("loading")
+            zeroHymns.previewDisplayName("zero hymns")
+            oneHymn.previewDisplayName("one hymn")
+            multipleHymns.previewDisplayName("multiple hymns")
+        }
+    }
+}
+#endif
