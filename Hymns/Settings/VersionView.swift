d698158448966310c2ae734684f88bd4207468ca
skywalkerdude <skywalkerdude@gmail.com>
2025-01-20 00:43:03 -0800

    Migrate VersionView to use Prefire Snapshots

--- Hymns/Settings/VersionView.swift
+++ Hymns/Settings/VersionView.swift
@@ -6,8 +6,18 @@ struct VersionView: View {
     @Environment(\.presentationMode) var presentationMode
     @ObservedObject private var coordinator: NavigationCoordinator
 
-    init(coordinator: NavigationCoordinator = Resolver.resolve()) {
+    let releaseVersion: String
+    let buildVersion: String
+    let systemVersion: String
+
+    init(coordinator: NavigationCoordinator = Resolver.resolve(),
+         releaseVersion: String = Bundle.main.releaseVersion,
+         buildVersion: String = Bundle.main.buildVersion,
+         systemVersion: String = UIDevice.current.systemVersion) {
         self.coordinator = coordinator
+        self.releaseVersion = releaseVersion
+        self.buildVersion = buildVersion
+        self.systemVersion = systemVersion
     }
 
     var body: some View {
@@ -16,27 +26,23 @@ struct VersionView: View {
             HStack {
                 Text("Release version")
                 Spacer()
-                Text("\(Bundle.main.releaseVersion)").foregroundColor(.gray)
+                Text("\(releaseVersion)").foregroundColor(.gray)
             }.padding()
             HStack {
                 Text("Build version")
                 Spacer()
-                Text("\(Bundle.main.buildVersion)").foregroundColor(.gray)
+                Text("\(buildVersion)").foregroundColor(.gray)
             }.padding()
             HStack {
                 Text("iOS version")
                 Spacer()
-                Text("\(UIDevice.current.systemVersion)").foregroundColor(.gray)
+                Text("\(systemVersion)").foregroundColor(.gray)
             }.padding()
             Spacer()
         }.hideNavigationBar()
     }
 }
 
-#if DEBUG
-struct VersionView_Previews: PreviewProvider {
-    static var previews: some View {
-        VersionView()
-    }
+#Preview {
+    VersionView(releaseVersion: "1.0.0", buildVersion: "12", systemVersion: "13.0")
 }
-#endif
