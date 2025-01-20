81193d392d5f7063685a1b2cf3b6d0731077849d
skywalkerdude <skywalkerdude@gmail.com>
2025-01-20 00:02:02 -0800

    Remove preview on HomeContainerView since all it doesn't really do a whole lot

--- Hymns/Home/HomeContainerView.swift
+++ Hymns/Home/HomeContainerView.swift
@@ -50,33 +50,3 @@ struct HomeContainerView: View {
         }
     }
 }
-
-#if DEBUG
-struct HomeContainerView_Previews: PreviewProvider {
-    static var previews: some View {
-        Group {
-            // preview all tabs
-            HomeContainerView(selectedTab: .search).previewDisplayName("Home tab")
-            HomeContainerView(selectedTab: .browse).previewDisplayName("Browse tab")
-            HomeContainerView(selectedTab: .favorites).previewDisplayName("Favorites tab")
-            HomeContainerView(selectedTab: .settings).previewDisplayName("Settings tab")
-            // preview localization
-            HomeContainerView().environment(\.locale, .init(identifier: "de")).previewDisplayName("German")
-            HomeContainerView().environment(\.locale, .init(identifier: "es")).previewDisplayName("Spanish")
-            // preview different sizes
-            HomeContainerView()
-                .previewDevice("iPhone 13")
-                .previewDisplayName("iPhone 13")
-            HomeContainerView()
-                .previewDevice("iPhone XS Max")
-                .previewDisplayName("iPhone XS Max")
-            HomeContainerView()
-                .previewLayout(.device)
-                .previewDevice("iPad Air 2")
-                .previewDisplayName("iPad Air 2")
-            // preview dark mode
-            HomeContainerView().environment(\.colorScheme, .dark).previewDisplayName("Dark mode")
-        }
-    }
-}
-#endif
